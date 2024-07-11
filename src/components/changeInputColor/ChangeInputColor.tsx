d489cd34dfe02ecc95da970a139d9389f81561cb
Paula Cano <paulacanoroman@gmail.com>
2024-07-11 18:30:40 +0200

    feat: state with color selected

--- src/components/changeInputColor/ChangeInputColor.tsx
+++ src/components/changeInputColor/ChangeInputColor.tsx
@@ -1,10 +1,16 @@
+import { useState } from "react"
 
 
 const ChangeInputColor = () => {
+
+    const [colorSelected, setColorSelected] = useState<string>()
   return (
     <>
     <h3>Change input color</h3>
-    <input type="color"></input>
+    
+    <b>The color selected is: {colorSelected}</b>
+    <br></br>
+    <input type="color" onChange={(e)=>{setColorSelected(e.target.value)}}></input>
     </>
   )
 }
