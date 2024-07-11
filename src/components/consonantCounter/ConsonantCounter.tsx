cf03edc1711f0677313dfeee9bb821181724e8e5
Paula Cano <paulacanoroman@gmail.com>
2024-07-11 18:25:31 +0200

    feat: counter consonant

--- src/components/consonantCounter/ConsonantCounter.tsx
+++ src/components/consonantCounter/ConsonantCounter.tsx
@@ -3,17 +3,31 @@ import { useState } from "react"
 
 const ConsonantCounter = () => {
 
-    const [sentence, setsentence] = useState<string>('')
- 
+    // const [sentence, setSentence] = useState<string>('')
+    const [counterConsonant, setCounterConsonant] = useState<number>(0)
+    const consonant : string[] = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'P', 'Q', 'R', 'S', 'T', 'V', 'X', 'Z', 'W', 'Y']
 
+    const handleCounterConsonant = (sentence:string)=>{
+      setCounterConsonant(0)
+       const letters = sentence.toUpperCase().split('');
+       letters.map((letra)=>{
+        console.log(letra,"-",)
+       if(consonant.some((consonante)=>consonante === letra)) {
+        setCounterConsonant(counterConsonant+1)
+       }
+
+       })
+       
+   
+    } 
 
 
   return (
     <>
     <h3>Number of consonants in your sentence</h3>
-    <input placeholder="Write a sentence and show number of consonants" onChange={(e)=>{setsentence(e.target.value)}}></input>
+    <input placeholder="Write a sentence and show number of consonants" onChange={(e)=>{handleCounterConsonant(e.target.value)}}></input>
     <br></br>
-    {sentence}
+    {counterConsonant}
     </>
   )
 }
